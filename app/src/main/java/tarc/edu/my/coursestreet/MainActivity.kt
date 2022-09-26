e7e70607d8398085ac4da4001f9a5be6e73155d7
Nyzr <nizarhamid80@gmail.com>
2022-09-26 19:26:34 +0800

    Added few fragments and nav_graph

--- app/src/main/java/tarc/edu/my/coursestreet/MainActivity.kt
+++ app/src/main/java/tarc/edu/my/coursestreet/MainActivity.kt
@@ -11,7 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         //nav drawer
 
