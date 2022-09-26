cb2a40bc64e68f92aeefe1782e7c6f725d53a7c1
Ho Wai Kit <68110992+1HoWK@users.noreply.github.com>
2022-09-26 18:13:03 +0800

    Added something related to nav graph

--- app/src/main/java/tarc/edu/my/coursestreet/MainActivity.kt
+++ app/src/main/java/tarc/edu/my/coursestreet/MainActivity.kt
@@ -2,10 +2,19 @@ package tarc.edu.my.coursestreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tarc.edu.my.coursestreet.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //nav drawer
+
+
     }
 }
