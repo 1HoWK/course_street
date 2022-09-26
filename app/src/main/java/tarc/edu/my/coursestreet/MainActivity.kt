29bdb9d071b7fc530ab12cd4e58b54be37dfcbaa
Nyzr <nizarhamid80@gmail.com>
2022-09-26 23:24:57 +0800

    change something not sure

--- app/src/main/java/tarc/edu/my/coursestreet/MainActivity.kt
+++ app/src/main/java/tarc/edu/my/coursestreet/MainActivity.kt
@@ -2,21 +2,33 @@ package tarc.edu.my.coursestreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import tarc.edu.my.coursestreet.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-
+    private val nav by lazy { supportFragmentManager.findFragmentById(R.id.host)!!.findNavController() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        //nav drawer
+        setupActionBarWithNavController(nav, binding.drawerLayout)
 
 
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
